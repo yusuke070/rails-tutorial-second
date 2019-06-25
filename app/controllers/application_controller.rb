fecc3bf9ad6edc2c129f3d0532d808b31462e8f4
yusuke <yusuke.akiba07@gmail.com>
2019-06-25 12:22:50 +0000

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def hello
+      render html:"¡Hola, mundo!"
+  end
+  def goodbye
+      render html: "よろしくみん"
+  end
+end
